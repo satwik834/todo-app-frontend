f373aa9cfaec04814755437a691f5781c9eb319a
saathwik <satwik2348@gmail.com>
2024-09-14 23:26:32 +0530

    added animation effects to the check button/box/whatever

--- src/Task.jsx
+++ src/Task.jsx
@@ -1,15 +1,33 @@
-import  Button  from "react-bootstrap/Button";
+import Button from "react-bootstrap/Button";
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck, faTimes } from "@fortawesome/free-solid-svg-icons";
 
-function Task(){
-    //code for done and deletion etc
-    
+function Task() {
+    const [checked, setChecked] = useState(false);
+    const [hover, setHover] = useState(false);
+    const [wasChecked, setWasChecked] = useState(false);
 
-    return(
+    function handleChecked() {
+        setWasChecked(checked);
+        setChecked(c => !c);
+    }
+
+    function Hovering() {
+        setHover(true);
+    }
+
+    function notHovering() {
+        setHover(false);
+        setWasChecked(false);
+    }
+
+    const showCheck = checked || (hover && !wasChecked);
+
+    return (
         <div className="todo">
-            <Button variant="dark" className="btn done-btn">
-                <FontAwesomeIcon icon={faCheck} />
+            <Button onMouseLeave={notHovering} onMouseEnter={Hovering} onClick={handleChecked} variant="dark" className="btn done-btn">
+            <FontAwesomeIcon icon={faCheck} className={`check-icon ${showCheck ? 'visible' : ''}`} />
             </Button>
             <p>Sample Task</p>
             <Button variant="dark" className="btn delete-btn">
